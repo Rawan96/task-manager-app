dc48eb03e91259bd297a5f953bb47b4b3e05472a
rawan <rawan.designer@hotmail.com>
2021-02-10 23:24:55 +0200

    validate the schema

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,23 +1,54 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true
 })
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 // const me = new User({
-//     name: 'Rawan',
-//     age: '24'
+//     name: '   Rawan  ',
+//     email: 'rawan.designer@hotmail.com   '
 // })
 
 // me.save().then(() => {
@@ -28,16 +59,18 @@ const User = mongoose.model('User', {
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const task = new Task({
     description: 'Build task app',
-    completed: false
 })
 
 task.save().then(() => {
