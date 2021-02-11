41ffd9dedf907e8b250e299fe0e2244196618d7e
rawan <rawan.designer@hotmail.com>
2021-02-11 23:19:48 +0200

    stroing the password

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt= require('bcrypt')
 
 const User = mongoose.model('User', {
     name: {
@@ -39,5 +40,17 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
