3ce07d26d1273fc9f18e52ab9eed5550082116a1
rawan <rawan.designer@hotmail.com>
2021-02-13 23:57:40 +0200

    Add the authentication middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router =  express.Router()
+const auth = require('../middleware/auth')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
