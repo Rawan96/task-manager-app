94bedd05482d0914173723c8fac67172065986d3
rawan <rawan.designer@hotmail.com>
2021-02-16 22:53:34 +0200

    Add file upload featuers to the app

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const router =  express.Router()
 const auth = require('../middleware/auth')
+
+const upload = multer({
+ dest: 'avatars'
+})
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -88,5 +93,10 @@ router.delete('/users/me', auth , async (req, res) => {
         res.status(500).send()
     }
 })
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+ res.send()
+})
+
 
 module.exports = router
