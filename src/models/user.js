660978b9d2e358eda926258e68b09659cee6040b
rawan <rawan.designer@hotmail.com>
2021-02-19 03:39:22 +0200

    Final edits

--- src/models/user.js
+++ src/models/user.js
@@ -71,7 +71,7 @@ userSchema.methods.toJSON = function () {
 }
 
 userSchema.methods.generateAuthToken = async function(){
-    const token = jwt.sign({_id: this._id.toString()}, process.env.secret)
+    const token = jwt.sign({_id: this._id.toString()}, process.env.JWT_SECRET)
     this.tokens = this.tokens.concat({token})
     await this.save()
 
