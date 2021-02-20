252f98a29c31947448173d4142c58beba63f21c0
rawan <rawan.designer@hotmail.com>
2021-02-20 23:38:09 +0200

    Add login tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,10 +1,37 @@
+require('env2')('./test.env');
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const user ={
+  name: 'Mohammed',
+  email:'moh@example.com',
+  password:'123456789'
+}
+
+beforeEach(async ()=>{
+  await User.deleteMany()
+  await new User(user).save()
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
-        name: 'Rawan',
-        email: 'rawan@example.com',
-        password: 'MyPass777!'
+        name: 'not',
+        email: 'not@example.com',
+        password: '123456ro'
     }).expect(201)
+})
+
+test('Should logging existing user', async()=>{
+  await request(app).post('/users/login').send({
+    email:user.email,
+    password:user.password
+  }).expect(200)
+})
+
+test('should not logging non existing user', async()=>{
+  await request(app).post('/users/login').send({
+    email:'non@example.com',
+    password:'123456789'
+  }).expect(400)
 })
