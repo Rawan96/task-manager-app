ec3d8657ab0aba2779d91a86f727ab22260a46fb
rawan <rawan.designer@hotmail.com>
2021-02-25 21:25:53 +0200

    setup eslint and prettier

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,8 @@
 require('env2')('./.env');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
--- src/emails/account.js
+++ src/emails/account.js
@@ -1,28 +1,27 @@
 require('env2')('./.env');
-const sgMail = require('@sendgrid/mail')
+const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.sendGridAPIKey)
+sgMail.setApiKey(process.env.sendGridAPIKey);
 
-const sendWelcomeEmail = (email , name)=>{
+const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
     from: 'rawan.designer@hotmail.com',
     subject: 'Welcome to the App, Hope you Have a Great Experience',
     text: `Welcome to the app ${name}, Let us know how you get along with the app`,
+  });
+};
 
-  })
-}
-
-const sendGoodByeEmail = (email, name)=>{
+const sendGoodByeEmail = (email, name) => {
   sgMail.send({
-    to:email,
-    from:'rawan.designer@hotmail.com',
-    subject:'Sorry to See You Go!',
-    text:`Goodbye ${name}, We Hope to See You Soon`
-  })
-}
+    to: email,
+    from: 'rawan.designer@hotmail.com',
+    subject: 'Sorry to See You Go!',
+    text: `Goodbye ${name}, We Hope to See You Soon`,
+  });
+};
 
 module.exports = {
   sendWelcomeEmail,
-  sendGoodByeEmail
-}
+  sendGoodByeEmail,
+};
